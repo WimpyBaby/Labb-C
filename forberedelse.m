42f0a9a2293935f0ba8004648c1819cfc08c68b7
WimpyBaby <83958291+WimpyBaby@users.noreply.github.com>
2023-02-23 00:46:29 +0100

    Add files via upload

--- forberedelse.m
+++ forberedelse.m
@@ -1,14 +1,52 @@
-h1 = 0.25;
-h2 = 0.125;
+% Q = @(t) (9+5*(cos(0.2*t)).^2);
+% c = @(t) (5*exp(-0.5*t)+2*exp(0.15*t));
+% f = @(t) Q(t).*c(t);
+% 
+% Shvec = [];
+% 
+% for n = [10 20 40]
+%     h = 6/n;
+%     t = 3:h:9;
+%     Sh = ((h/3)*(sum(f(t(2:2:end-1)))*4+(sum(f(t(3:2:end-2)))*2+f(t(1))+f(t(end)))));
+%     Shvec = [Shvec, Sh];
+% end
+% 
+% nog = log2(abs(Shvec(2)-Shvec(1))/abs(Shvec(3)-Shvec(2)));
 
-x = 0:0.125:1;
+% psmax = 75000;
+% psmin = 100000;
+% ks = 0.045;
+% pfmax = 300000;
+% kf = 0.08;
+% p0 = 10000;
+% 
+% ps = @(t) psmax.*exp(-ks.*t) + psmin;
+% pf = @(t) pfmax/(1+((pfmax/p0)-1).*exp(-kf.*t));
+% 
+% f = @(t) pfmax./(1+((pfmax./(p0))-1)*exp(-kf.*t)) - 1.2*(psmax*exp(-ks.*t)+psmin);
+% fp = @(t) 4050*exp(0.045.*t) + ((696000*exp(-0.08.*t))./((29*exp(-0.08.*t)+1).^2));
+% 
+% t = 35;
+% tol = 1e-8;
+% diff = 1
+% 
+% tvec = [ t ];
+% 
+% while abs(diff) > tol
+%     tny = t - f(t)/fp(t);
+%     diff = abs(tny - t);
+%     t = tny;
+%     tvec = [tvec, t];
+% end
+% 
+% diffT1 = abs(tvec(2:end)-tvec(1:end-1));
+% diffT2 = abs(diffT1(2:end)./diffT1(1:end-1).^2);
+% diffT2 = diffT2';
 
-f = [2.6 2.08 1.72 1.45 1.26 1.13 1.04 0.97 0.92];
+h = 0.25;
 
-T2h = (h1/2).*(f(1).*x(1) + 2.*(f(3).*x(3) + f(5).*x(5) + f(7).*x(7)) + f(9)*x(9)); 
+x = [0 0.125 0.25 0.375 0.5 0.625 0.75 0.875 1];
+b = [2.6 2.08 1.72 1.45 1.26 1.13 1.04 0.97 0.92];
+f = b.*x;
 
-Th = (h2/2).*(f(1).*x(1) + 2.*(f(2).*x(2) + f(3).*x(3) + f(4).*x(4) + f(5).*x(5) + f(6).*x(6) + f(7).*x(7) + f(8).*x(8)) + f(9).*x(9));
-
-Tstar = (4.*Th - T2h)./3;
-
-disp(Tstar)
+T2h = (h/2).*(f(1) + 2.*f(3) + 2.*f(5) + 2.*f(7) + f(9));
--- newton_method_lab_c.m
+++ newton_method_lab_c.m
@@ -1,80 +1,116 @@
 clear all;
 
-% psmax = 75000;
-% psmin = 100000;
-% ks = 0.045;
-% pfmax = 300000;
-% kf = 0.08;
-% p0 = 10000;
-% 
-% ps = @(t) psmax.*exp(-ks.*t) + psmin;
-% pf = @(t) pfmax/(1+((pfmax/p0)-1).*exp(-kf.*t));
+psmax = 75000;
+psmin = 100000;
+ks = 0.045;
+pfmax = 300000;
+kf = 0.08;
+p0 = 10000;
+
+ps = @(t) psmax.*exp(-ks.*t) + psmin;
+pf = @(t) pfmax/(1+((pfmax/p0)-1).*exp(-kf.*t));
 
 %befolkningsökningen kan defineras som Pf - Ps
 % funktion ska lösas för när förort är 20% större än stan (pf - 1,2ps = 0)
 
-% f = @(t) pfmax./(1+((pfmax./(p0))-1)*exp(-kf.*t)) - 1.2*(psmax*exp(-ks.*t)+psmin);
-% fp = @(t) 4050*exp(0.045.*t) + ((696000*exp(-0.08.*t))./((29*exp(-0.08.*t)+1).^2));
-% 
-% tol = 1e-8;
-% delta_t = 1;
-% t = 20;
-% 
-% guess = [];
-% 
-% while delta_t > tol
-%     tnext = t - f(t)/fp(t);
-%     delta_t = abs(tnext-t);
-%     t = tnext;
-%     guess = [guess, t];
-% end
-% 
-% Hn = abs(guess(2:end)-guess(1:end-1));
-% konv = 
+f = @(t) pfmax./(1+((pfmax./(p0))-1)*exp(-kf.*t)) - 1.2*(psmax*exp(-ks.*t)+psmin);
+fp = @(t) 4050*exp(0.045.*t) + ((696000*exp(-0.08.*t))./((29*exp(-0.08.*t)+1).^2));
+
+tol = 1e-8;
+delta_t = 1;
+t = 20;
+
+guess = [];
+
+while abs(delta_t) > tol
+    tnext = t - f(t)/fp(t);
+    delta_t = abs(tnext-t);
+    t = tnext;
+    guess = [guess, t];
+end
+
+% diffT1 = abs(guess(2:end)-guess(1:end-1));
+% diffT2 = abs(diffT1(2:end)./(diffT1(1:end-1).^2));
+
+kon = log2(abs(guess(2)-guess(1))/(abs(guess(3)-guess(2))).^2);
 
 % --------------------------------------------------------------------------
 
-load("STOCKHOLMSDATA.mat");
+% load("STOCKHOLMSDATA.mat");
+% 
+% c3 = [];
+% c1 = repelem(1, 70080);
+% c1 = c1';
+% 
+% c2 = 0:1:70079;
+% c2 = c2';
+% %c3 = sin(2.*pi./(365*24).*((0:1:70080);
+% 
+% for n = 0:70079
+%     a = sin(((2*pi)/(365*24))*(n-(117*24)));
+%     c3 = [c3, a];
+% end
+% 
+% c3 = c3';
+% 
+% t = 0:1:70080;
+% t = t';
+% 
+% Ainterp = [c1 c2 c3];
+% 
+% cinterp = Ainterp\Tm;
+% x = 0:1:70079;
+% % T = cinterp(1) + cinterp(2).*x + cinterp(3).*sin(((2*pi)/(365*24))*(x-240));
+% T = @(x) cinterp(1) + cinterp(2).*x + cinterp(3).*sin(((2*pi)/(365*24))*(x-(117*24)));
+% % T = T';
+% res = Ainterp * cinterp - Tm;
+% resnorm = norm(res).^2;
+% disp(resnorm)
 
-c3 = [];
-c1 = repelem(1, 70080);
-c1 = c1';
 
-c2 = 0:1:70079;
-c2 = c2';
-%c3 = sin(2.*pi./(365*24).*((0:1:70080);
+% % x = 0:1:70080;
+% % i = lsqr(Ainterp, Tm);
+% % T = i(1) + i(2) + i(3).*sin(((2*pi)/(365*24))*(x-240));
 
-for n = 0:70079
-    a = sin(((2*pi)/(365*24))*(n-240));
-    c3 = [c3, a];
-end
+% plot(x, T(x))
 
-c3 = c3';
-
-t = 0:1:70080;
-t = t';
-
-Ainterp = [c1 c2 c3];
-
-cinterp = Ainterp\Tm;
-x = 0:1:70079;
-T = cinterp(1) + cinterp(2).*x + cinterp(3).*sin(((2*pi)/(365*24))*(x-240));
-T = T';
-residual = abs(norm((Tm-T).^2));
-
-% x = 0:1:70080;
-% i = lsqr(Ainterp, Tm);
-% T = i(1) + i(2) + i(3).*sin(((2*pi)/(365*24))*(x-240));
-
-plot(x, T)
-
-% xlabel("Tid i timmar")
+% xlabel("Tid")
 % ylabel("Temperatur i C")
 
-%T = @(t) c(1) + c(2).*t + c(3).*sin(2.*pi.*(t-240));
+% %T = @(t) c(1) + c(2).*t + c(3).*sin(2.*pi.*(t-240));
+% 
+% %Tmc = y
 
-%Tmc = y
+% --------------------------------------------------------------------------
 
+% Q = @(t) (9+5*(cos(0.2*t)).^2);
+% c = @(t) (5*exp(-0.5*t)+2*exp(0.15*t));
+% f = @(t) Q(t).*c(t);
+% 
+% x0 = 3;
+% x = 9;
 
+% n = [10 20 40];
+% n = 10;
 
+% h = (x-x0)/n;
+% t = x0:h:x;
+% t2 = x0:0.3:x;
+% t4 = x0:0.15:x;
 
+% intvalues = [];
+% 
+% for i = n
+%     h = (x-x0)/i;
+%     t = x0:h:x;
+%     TSim = ((h/3)*(sum(f(t(2:2:end-1)))*4+(sum(f(t(3:2:end-2)))*2+f(t(1))+f(t(end)))));
+%     intvalues = [intvalues, TSim];
+% end
+
+% Tsim = (h/3).*(f(t(1))+(sum(f(t(2:2:end-1)))*4 +(sum(f(t(3:2:end-2)))*2 + f(t(end)))));
+% T2Sim = (h/6).*(f(t2(1))+(sum(f(t2(2:2:end-1)))*4 +(sum(f(t2(3:2:end-2)))*2 + f(t2(end)))));
+% T4Sim = (h/12).*(f(t4(1))+(sum(f(t4(2:2:end-1)))*4 +(sum(f(t4(3:2:end-2)))*2 + f(t4(end)))));
+% 
+% intvalues = [Tsim, T2Sim, T4Sim];
+% 
+% nog = log2(abs(intvalues(2)-intvalues(1))/abs(intvalues(3)-intvalues(2)));
